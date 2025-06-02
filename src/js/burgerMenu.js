1359f7782e10c903d582fd5b57c9d259084d4821
Marakas <igorpetriv.bsc@gmail.com>
2025-06-02 21:18:21 +0300

    mobile menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,36 @@
-// const openBtnEl = document.querySelector('[data-action="open"]');
-// const closeBtnEl = document.querySelector('[data-action="close"]');
-// const burgerMenuEl = document.querySelector('[data-visible]');
+const openBtnEl = document.querySelector('[data-action="open"]');
+const closeBtnEl = document.querySelector('[data-action="close"]');
+const burgerMenuEl = document.querySelector('[data-visible]');
 
-// openBtnEl.addEventListener('click', e => {
-//   burgerMenuEl.dataset.visible = 'open';
-// });
+// OPEN MENU
+openBtnEl.addEventListener('click', () => {
+  burgerMenuEl.dataset.visible = 'open';
+});
 
-// closeBtnEl.addEventListener('click', e => {
-//   burgerMenuEl.dataset.visible = 'close';
-// });
+// CLOSE BTN
+closeBtnEl.addEventListener('click', () => {
+  burgerMenuEl.dataset.visible = 'close';
+});
+
+// CLICK OUTSIDE MENU
+burgerMenuEl.addEventListener('click', event => {
+  if (event.target === burgerMenuEl) {
+    burgerMenuEl.dataset.visible = 'close';
+  }
+});
+
+// SWIPE
+let touchStartX = 0;
+
+burgerMenuEl.addEventListener('touchstart', e => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+burgerMenuEl.addEventListener('touchend', e => {
+  const touchEndX = e.changedTouches[0].screenX;
+  const deltaX = touchEndX - touchStartX;
+
+  if (deltaX > 50) {
+    burgerMenuEl.dataset.visible = 'close';
+  }
+});
