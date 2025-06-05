07ad7f8127a931958e8b04b39e0c778dba05c93e
Marakas <igorpetriv.bsc@gmail.com>
2025-06-05 17:08:49 +0300

    Fixed little problems

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -3,22 +3,24 @@ const acceptCookies = document.querySelector('[data-cookieBtn="accept"]');
 const declineCookies = document.querySelector('[data-cookieBtn="decline"]');
 
 try {
-  let value = JSON.parse(localStorage.getItem('ACCEPTED'));
+  let value = JSON.parse(localStorage.getItem('cookies'));
 
-  value
-    ? (cookiesModalEl.dataset.isCookies = 'close')
-    : (cookiesModalEl.dataset.isCookies = 'open');
+  if (value) {
+    cookiesModalEl.dataset.isCookies = 'close';
+  } else {
+    cookiesModalEl.dataset.isCookies = 'open';
+  }
 } catch {
   cookiesModalEl.dataset.isCookies = 'open';
 }
 
 cookiesModalEl.addEventListener('click', event => {
   if (event.target === acceptCookies) {
-    localStorage.setItem('ACCEPTED', JSON.stringify(true));
+    localStorage.setItem('cookies', JSON.stringify('accepted'));
     cookiesModalEl.dataset.isCookies = 'close';
   }
   if (event.target === declineCookies) {
-    localStorage.setItem('ACCEPTED', JSON.stringify(false));
+    localStorage.setItem('cookies', JSON.stringify('declined'));
     cookiesModalEl.dataset.isCookies = 'close';
   }
 });
